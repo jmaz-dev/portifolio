086396f82ee388a02e78b69eacfb992619ab8c31
jmaraujo93 <jmaraujo93@gmail.com>
2022-07-05 18:01:22 -0300

    cc

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,12 @@
+import { initReactI18next } from 'react-i18next';
+import {LanguageDetector} from 'i18next-browser-languagedetector';
+
 module.exports = {
   i18n: {
+    initReactI18next,
     defaultLocale: "en",
     locales: ["en", "pt"],
+    LanguageDetector,
+    debug: true,
   },
 };
