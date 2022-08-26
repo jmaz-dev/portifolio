c814645219101593f7b065e1b79f43860f0e84b5
jmaraujo93 <jmaraujo93@gmail.com>
2022-08-25 21:17:18 -0300

    whatsapp chat

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,17 @@ import Navbar from "../components/Navbar";
 import { appWithTranslation } from "next-i18next";
 import App from "next/app";
 import nextI18NextConfig from "../next-i18next.config.js";
+import Script from "next/script";
 
 class MyApp extends App {
+ 
   render() {
     const { Component, pageProps } = this.props;
     return (
       <>
         <Navbar />
         <Component {...pageProps} />
+        <Script src='https://cdn.leadster.com.br/neurolead/neurolead.min.js'></Script>
       </>
     );
   }
